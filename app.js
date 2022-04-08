563edaf12700784c6fadc48da7e0641b9595b35e
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-04-07 21:41:07 -0400

    add .then

--- app.js
+++ app.js
@@ -30,7 +30,12 @@ const promptUser = () => {
     ]);
 };
 
-const promptProject = () => {
+const promptProject = portfolioData => {
+    // If there's no 'projects' array property, create one 
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+    
     console.log(`
     =================
     Add a New Project
@@ -70,10 +75,19 @@ const promptProject = () => {
             message: 'Would you like to enter another project?',
             default: false
         }
-    ]);
-}
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+            if (projectData.confirmAddProject) {
+                return promptProject(portfolioData);
+            } else {
+                return portfolioData;
+            }
+        })      
+};
 
 promptUser()
-    .then(answers => console.log(answers))
     .then(promptProject)
-    .then(projectAnswers => console.log(projectAnswers));
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
