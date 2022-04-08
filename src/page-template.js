71a3c27482c1aefd5792088fdef90ce8a8752b75
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-04-08 17:33:07 -0400

    refactor generateProjects

--- src/page-template.js
+++ src/page-template.js
@@ -10,32 +10,48 @@ const generateAbout = aboutText => {
     </section>`;
 };
 
-const generateProjects = projectsArr => {
-    const projectHtmlArr = projectsArr.map(({ name, description, languages, link }) => {
-        return `
+const generateProjects = (projectsArr) => {
+    return `
+        <section class="my-3" id="portfolio">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+        <div class="flex-row justify-space-between">
+        ${projectsArr
+            .filter(({ feature }) => feature)
+            .map(({ name, description, languages, link }) => {
+            return `
+            <div class="col-12 mb-2 bg-dark text-light p-3">
+                <h3 class="portfolio-item-title text-light">${name}</h3>
+                <h5 class="portfolio-languages">
+                    Built With:
+                    ${languages.join(", ")}
+                </h5>
+                <p>${description}</p>
+                <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+            `;
+            })
+            .join("")}
+
+        ${projectsArr
+            .filter(({ feature }) => !feature)
+            .map(({ name, description, languages, link }) => {
+            return `
             <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
                 <h3 class="portfolio-item-title text-light">${name}</h3>
                 <h5 class="portfolio-languages">
                     Built With:
-                    ${languages.join(', ')}
+                    ${languages.join(", ")}
                 </h5>
                 <p>${description}</p>
                 <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
             </div>
-        `;
-    });
-    
-    
-    return `
-        <section class="my-3" id="portfolio">
-            <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-            <div class="flex-row justify-space-between">
-                ${projectHtmlArr.join("")}
-            </div>
+            `;
+            })
+            .join("")}
+        </div>
         </section>
     `;
 };
-
 module.exports = templateData => {
     // destructure projects and about data from templateData based on their property key names
     const { projects, about, ...header } = templateData;
